95c071e9704aa6de8a6c7b89636db3b7d190c053
Nahuel Araujo <nahuargentina@gmail.com>
2020-10-08 22:33:20 -0300

    Modulo V 1.0.0

--- modulo.js
+++ modulo.js
@@ -1,34 +1,14 @@
-
-// Cargar el modulo HTTP
-var http = require('http');
-
 var dashboard = require('./dashboard.js');
  
-// Configurar una respuesta HTTP para todas las peticiones
-function onRequest(request, response) {
-  console.log("Peticion Recibida.");
-  console.log(response);
-  dashboard.Principal()
-  response.end();
-}
- 
+const express = require('express')
+const app = express()
+const fs = require("fs")
 
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-app.use(bodyParser.json());
-app.post('/', (req, res) => {
-    console.log(req.body);
-    res.send('Data received');
+
+app.get('/', function (req, res) {
+  res.send('Principal ejecutado')
+  res.send(dashboard.Principal())
 })
 
 
-var server = http.createServer(onRequest);
-
-
-
-// Escuchar al puerto 8080
-server.listen(8080);
- 
-// Poner un mensaje en la consola
-console.log("Servidor funcionando en http://localhost:8080/");
+app.listen(3000)
